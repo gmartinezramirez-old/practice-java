2addda8c7b33734900db3f66998c61b9a75b4e83
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2017-02-08 21:57:55 -0300

    CLEAN: cleanup code, specially identation. TODO: FIX problem with .editorconfig in nvim

--- src/datastructures/hashMapExample.java
+++ src/datastructures/hashMapExample.java
@@ -4,12 +4,12 @@ import java.util.Map;
 public class hashMapExample {
     public static void main(String[] args) {
         Map<String, Integer> hashTableExample = new HashMap<>();
-	hashTableExample.put("one", 1);
-	hashTableExample.put("two", 2);
-	hashTableExample.put("three", 3);
-	for (Map.Entry<String, Integer> entry : hashTableExample.entrySet()) {
-	    System.out.println("Key = " + entry.getKey() 
-	        + ", Value = " + entry.getValue());
+        hashTableExample.put("one", 1);
+        hashTableExample.put("two", 2);
+        hashTableExample.put("three", 3);
+        for (Map.Entry<String, Integer> entry : hashTableExample.entrySet()) {
+            System.out.println("Key = " + entry.getKey() 
+			    + ", Value = " + entry.getValue());
 	}
     }
 
--- src/math/fibonacciDynamic.java
+++ src/math/fibonacciDynamic.java
@@ -1,25 +1,27 @@
 
-
 public class fibonacciDynamic {
-    public static int fibonacci(int index) {
-        if (index == 0) {
+    public static int calculateFibonacci(int index) {
+        if (index == 0)
             return 0;
-	}
 
-	if (index == 1) {
+        if (index == 1)
             return 1;
-	}
 
-	int prePre = 0;
-	int pre = 1;
-	int result = 0;
-	for (int i = 1; i < index; i++) {
+        int prePre = 0;
+        int pre = 1;
+        int result = 0;
+        for (int i = 1; i < index; i++) {
             result = prePre + pre;
-	    prePre = pre;
-	    pre = result;
-	}
-	return result;
+            prePre = pre;
+            pre = result;
+        }
+        return result;
     }
 
+    public static void main(String[] args) {
+        int index = 3;
+	int fibonacciResult = calculateFibonacci(index);
+        System.out.println(calculateFibonacci);
+    }
 
 }
